1c746f75e8859ebf4a1a7466a82d1cd2c43e9fca
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-02-28 16:57:54 +0000

    set a margin to the body

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App container">
+    <div className="App">
       <h1 className="App-header">Dictionary</h1>
     </div>
   );
