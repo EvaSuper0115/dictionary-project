c713e627ee4cb65096b2a7346e5aa960365fbb9b
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-02-28 16:49:20 +0000

    initial setup
    -added react
    -added bootstrap css and JS cdn

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App container">
+      <h1 className="App-header">Dictionary</h1>
+    </div>
+  );
+}
+
+export default App;
