ea42c57458c799fe1dc9f704f5844288da76a31e
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-01 14:55:57 +0000

    mapping definitions for definitions and examples

--- src/Meanings.js
+++ src/Meanings.js
@@ -6,15 +6,23 @@ export default function Meanings(props) {
     <div className="Meanings">
       <div className=" widget">
         <div className="partOfSpeech"> {props.meaning.partOfSpeech}</div>
-        <div className="meaning-content-text">
-          {props.meaning.definitions[0].definition}
-        </div>
-        <div className="meaning-content-text example-sentence">
-          {props.meaning.definitions[0].example}
-        </div>
+        {props.meaning.definitions.map(function (definition, index) {
+          return (
+            <div className="group-content" key={index}>
+              <div className="meaning-content-text">
+                {definition.definition}
+              </div>
+              <div className="meaning-content-text example-sentence">
+                {definition.example}
+              </div>
+            </div>
+          );
+        })}
       </div>
 
-      <div>Similar :</div>
+      <div>
+        <small>Synonyms (if any):</small>
+      </div>
       <div className="overflow-x-auto">
         {props.meaning.synonyms.map(function (synonyms, index) {
           return (
--- src/Results.js
+++ src/Results.js
@@ -3,7 +3,7 @@ import Meanings from "./Meanings.js";
 export default function Results(props) {
   return (
     <div className="Results">
-      <div className="word"> {props.results.word}</div>
+      <div className="word gradient-text"> {props.results.word}</div>
       <div className="phonetic"> {props.results.phonetic}</div>
       {props.results.meanings.map(function (meaning, index) {
         return (
