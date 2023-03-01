c81bba1e6a80cb7ffc55bfccc85f494af98d664f
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-01 14:17:54 +0000

    added examples

--- src/Meanings.js
+++ src/Meanings.js
@@ -4,7 +4,17 @@ export default function Meanings(props) {
   console.log(props.meaning);
   return (
     <div className="Meanings">
-      <div className="partOfSpeech widget">{props.meaning.partOfSpeech}</div>
+      <div className=" widget">
+        <div className="partOfSpeech"> {props.meaning.partOfSpeech}</div>
+        <div className="meaning-content-text">
+          {props.meaning.definitions[0].definition}
+        </div>
+        <div className="meaning-content-text example-sentence">
+          {props.meaning.definitions[0].example}
+        </div>
+      </div>
+
+      <div>Similar :</div>
       <div className="overflow-x-auto">
         {props.meaning.synonyms.map(function (synonyms, index) {
           return (
