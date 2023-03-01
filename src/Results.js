eb8168473ccb9560fdf1ea896ef93983ec4d778c
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-01 14:30:33 +0000

    added pheonics

--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,10 @@
 import React from "react";
 import Meanings from "./Meanings.js";
 export default function Results(props) {
-  let word = props.results.word;
-
   return (
     <div className="Results">
-      <div className="word"> {word}</div>
+      <div className="word"> {props.results.word}</div>
+      <div className="phonetic"> {props.results.phonetic}</div>
       {props.results.meanings.map(function (meaning, index) {
         return (
           <div key={index}>
