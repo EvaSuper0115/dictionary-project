132b421392d7f9c0ed3794c01ad2b0f3c0d8f8bb
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-03-03 14:25:18 +0000

    added 6 images accroding to search

--- src/Photos.js
+++ src/Photos.js
@@ -2,25 +2,39 @@ import React from "react";
 
 export default function Photos(props) {
   return (
-    <div className="card">
-      <img
-        className="photos-adjustments card-img"
-        src={props.images.photos[0].src.landscape}
-        alt={props.images.photos[0].alt}
-      ></img>
-      <div className="card-img-overlay">
-        <a
-          href={props.images.photos[0].photographer_url}
-          target="_blank"
-          rel="noreferrer"
-          title={`${props.images.photos[0].photographer}'s Page`}
-        >
-          {" "}
-          <p className="card-text">
-            Photo by : {props.images.photos[0].photographer}
-          </p>
-        </a>
-      </div>
+    <div className="photo-grid">
+      {props.images.photos.map((photo, index) => {
+        return (
+          <div key={index} className="card photos-adjustments">
+            <img
+              className="card-img"
+              src={photo.src.landscape}
+              alt={photo.alt}
+            ></img>
+
+            <div className="card-img-overlay">
+              <a
+                href={photo.photographer_url}
+                target="_blank"
+                rel="noreferrer"
+                title={`${photo.photographer}'s Page`}
+              >
+                {" "}
+                <p className="card-text">Photo by : {photo.photographer}</p>
+              </a>
+              <a
+                href={photo.url}
+                title="original"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                <p className="card-text">See original</p>
+              </a>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
